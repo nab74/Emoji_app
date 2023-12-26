975dc9dbd0572d75a9e2dc05e4c3909800842eaf
Nabhanya Sharma <@nab74>
2023-12-26 15:25:23 -0500

    Setup till Sign-in

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse} from "next/server";
+
+export default withClerkMiddleware(()=>{
+    console.log("middleware running");
+    return  NextResponse.next();
+});
+
+export const config = {
+    marcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
